210cdde7b2e7c2e902ba1e67fc278f38e577a2b4
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-24 18:10:15 +0600

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int num1,num2;
+    printf("enter two integer=");
+    scanf("%d %d",&num1,&num2);
+
+    printf("the numbers are %d %d",num1, num2);
+    return 0;
+}
